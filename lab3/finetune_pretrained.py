619cfb81813ca3e6ece5a5c1585a2a3f94df9672
Markosgi <94316555+Markosgi@users.noreply.github.com>
2023-07-14 23:37:39 +0300

    final

--- lab3/finetune_pretrained.py
+++ lab3/finetune_pretrained.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import LabelEncoder
 from utils.load_datasets import load_MR, load_Semeval2017A
 
 
-DATASET = 'MR'  # 'MR' or 'Semeval2017A'
-PRETRAINED_MODEL = 'bert-base-cased'
+#DATASET = 'MR'  # 'MR' or 'Semeval2017A'
+PRETRAINED_MODELS = ['bert-base-cased', 'roberta-base', 'distilbert-base-uncased']
 
 
 metric = evaluate.load("accuracy")
@@ -34,57 +34,56 @@ def prepare_dataset(X, y):
 
 if __name__ == '__main__':
 
-    # load the raw data
-    if DATASET == "Semeval2017A":
-        X_train, y_train, X_test, y_test = load_Semeval2017A()
-    elif DATASET == "MR":
-        X_train, y_train, X_test, y_test = load_MR()
-    else:
-        raise ValueError("Invalid dataset")
+    for DATASET in ("MR", "Semeval2017A"):
+        if DATASET == "Semeval2017A":
+            X_train, y_train, X_test, y_test = load_Semeval2017A()
+        elif DATASET == "MR":
+            X_train, y_train, X_test, y_test = load_MR()
 
-    # encode labels
-    le = LabelEncoder()
-    le.fit(list(set(y_train)))
-    y_train = le.transform(y_train)
-    y_test = le.transform(y_test)
-    n_classes = len(list(le.classes_))
+        # encode labels
+        le = LabelEncoder()
+        le.fit(list(set(y_train)))
+        y_train = le.transform(y_train)
+        y_test = le.transform(y_test)
+        n_classes = len(list(le.classes_))
 
-    # prepare datasets
-    train_set = prepare_dataset(X_train, y_train)
-    test_set = prepare_dataset(X_test, y_test)
+        # prepare datasets
+        train_set = prepare_dataset(X_train, y_train)
+        test_set = prepare_dataset(X_test, y_test)
 
-    # define model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
-    model = AutoModelForSequenceClassification.from_pretrained(
-        PRETRAINED_MODEL, num_labels=n_classes)
+        for PRETRAINED_MODEL in PRETRAINED_MODELS:
+            # define model and tokenizer
+            tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
+            model = AutoModelForSequenceClassification.from_pretrained(
+                PRETRAINED_MODEL, num_labels=n_classes)
 
-    # tokenize datasets
-    tokenized_train_set = train_set.map(tokenize_function)
-    tokenized_test_set = test_set.map(tokenize_function)
+            # tokenize datasets
+            tokenized_train_set = train_set.map(tokenize_function)
+            tokenized_test_set = test_set.map(tokenize_function)
 
-    # TODO: Main-lab-Q7 - remove this section once you are ready to execute on a GPU
-    #  create a smaller subset of the dataset
-    n_samples = 40
-    small_train_dataset = tokenized_train_set.shuffle(
-        seed=42).select(range(n_samples))
-    small_eval_dataset = tokenized_test_set.shuffle(
-        seed=42).select(range(n_samples))
+            # TODO: Main-lab-Q7 - remove this section once you are ready to execute on a GPU
+            #  create a smaller subset of the dataset
+            n_samples = 40
+            small_train_dataset = tokenized_train_set.shuffle(
+                seed=42).select(range(n_samples))
+            small_eval_dataset = tokenized_test_set.shuffle(
+                seed=42).select(range(n_samples))
 
-    # TODO: Main-lab-Q7 - customize hyperparameters once you are ready to execute on a GPU
-    # training setup
-    args = TrainingArguments(
-        output_dir="output",
-        evaluation_strategy="epoch",
-        num_train_epochs=5,
-        per_device_train_batch_size=8
-    )
-    trainer = Trainer(
-        model=model,
-        args=args,
-        train_dataset=small_train_dataset,
-        eval_dataset=small_eval_dataset,
-        compute_metrics=compute_metrics,
-    )
+            # TODO: Main-lab-Q7 - customize hyperparameters once you are ready to execute on a GPU
+            # training setup
+            args = TrainingArguments(
+                output_dir="output",
+                evaluation_strategy="epoch",
+                num_train_epochs=5,
+                per_device_train_batch_size=8
+            )
+            trainer = Trainer(
+                model=model,
+                args=args,
+                train_dataset=small_train_dataset,
+                eval_dataset=small_eval_dataset,
+                compute_metrics=compute_metrics,
+            )
 
-    # train
-    trained_model = trainer.train()
+            # train
+            trained_model = trainer.train()
--- lab3/transfer_pretrained.py
+++ lab3/transfer_pretrained.py
@@ -4,11 +4,6 @@ from tqdm import tqdm
 from utils.load_datasets import load_MR, load_Semeval2017A
 from training import get_metrics_report
 
-# DATASET = 'MR'
-# PRETRAINED_MODEL = 'siebert/sentiment-roberta-large-english'
-
-DATASET = 'Semeval2017A'
-PRETRAINED_MODEL = 'cardiffnlp/twitter-roberta-base-sentiment'
 
 
 LABELS_MAPPING = {
@@ -20,33 +15,63 @@ LABELS_MAPPING = {
         'LABEL_0': 'negative',
         'LABEL_1': 'neutral',
         'LABEL_2': 'positive',
-    }
+    },
+    'lxyuan/distilbert-base-multilingual-cased-sentiments-student': {
+        'negative': 'negative',
+        'neutral': 'neutral',
+        'positive': 'positive',
+    },
+    'textattack/bert-base-uncased-imdb': {
+        'LABEL_0': 'negative',
+        'LABEL_1': 'positive',
+    },
+    'textattack/roberta-base-imdb': {
+        'LABEL_0': 'negative',
+        'LABEL_1': 'positive',
+    },
+    'finiteautomata/bertweet-base-sentiment-analysis': {
+        'NEG': 'negative',
+        'NEU': 'neutral',
+        'POS': 'positive',
+    },
 }
 
+PRETRAINED_MODELS_MR = [
+    'siebert/sentiment-roberta-large-english',
+    'textattack/bert-base-uncased-imdb',
+    'textattack/roberta-base-imdb'
+]
+PRETRAINED_MODELS_SEMEVAL = [
+    'cardiffnlp/twitter-roberta-base-sentiment',
+    'lxyuan/distilbert-base-multilingual-cased-sentiments-student',
+    'finiteautomata/bertweet-base-sentiment-analysis'
+]
+
 if __name__ == '__main__':
-    # load the raw data
-    if DATASET == "Semeval2017A":
-        X_train, y_train, X_test, y_test = load_Semeval2017A()
-    elif DATASET == "MR":
-        X_train, y_train, X_test, y_test = load_MR()
-    else:
-        raise ValueError("Invalid dataset")
 
-    # encode labels
-    le = LabelEncoder()
-    le.fit(list(set(y_train)))
-    y_train = le.transform(y_train)
-    y_test = le.transform(y_test)
-    n_classes = len(list(le.classes_))
+    for DATASET in ("MR", "Semeval2017A"):
+        if DATASET == "Semeval2017A":
+            PRETRAINED_MODELS = PRETRAINED_MODELS_SEMEVAL
+            X_train, y_train, X_test, y_test = load_Semeval2017A()
+        elif DATASET == "MR":
+            PRETRAINED_MODELS = PRETRAINED_MODELS_MR
+            X_train, y_train, X_test, y_test = load_MR()
 
-    # define a proper pipeline
-    sentiment_pipeline = pipeline("sentiment-analysis", model=PRETRAINED_MODEL)
+        # encode labels
+        le = LabelEncoder()
+        le.fit(list(set(y_train)))
+        y_train = le.transform(y_train)
+        y_test = le.transform(y_test)
+        n_classes = len(list(le.classes_))
 
-    y_pred = []
-    for x in tqdm(X_test):
-        # TODO: Main-lab-Q6 - get the label using the defined pipeline
-        label = ...
-        y_pred.append(LABELS_MAPPING[PRETRAINED_MODEL][label])
+        for PRETRAINED_MODEL in PRETRAINED_MODELS:
+            # define a proper pipeline
+            sentiment_pipeline = pipeline("sentiment-analysis", model=PRETRAINED_MODEL)
 
-    y_pred = le.transform(y_pred)
-    print(f'\nDataset: {DATASET}\nPre-Trained model: {PRETRAINED_MODEL}\nTest set evaluation\n{get_metrics_report([y_test], [y_pred])}')
+            y_pred = []
+            for x in tqdm(X_test):
+                label = sentiment_pipeline(x)[0]['label']
+                y_pred.append(LABELS_MAPPING[PRETRAINED_MODEL][label])
+
+            y_pred = le.transform(y_pred)
+            print(f'\nDataset: {DATASET}\nPre-Trained model: {PRETRAINED_MODEL}\nTest set evaluation\n{get_metrics_report([y_test], [y_pred])}')
